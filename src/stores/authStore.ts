537950a5544367a606bbb83ee6d675e6a46812d0
Sungang Kim <vnvndldl@naver.com>
2024-12-25 03:38:35 +0900

    base url 수정

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -18,7 +18,7 @@ export const useAuthStore = defineStore('auth', {
       localStorage.removeItem('kakao_token');
       this.isAuthenticated = false;
       this.userId = null;
-      window.location.href = './signin'
+      window.location.href = './'
     }
   }
 });
